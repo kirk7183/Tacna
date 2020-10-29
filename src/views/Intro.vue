3cbfdca4f9ec02f946ae09903554eb0073210da7
kirk7183 <vasicigorjsp@gmail.com>
2020-10-29 14:22:58 +0100

    still testing

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,73 +1,133 @@
 <template>
-  <div class="home">
+  <v-container class="home" fluid>
     <div class="top">
-      <svg
-        version="1.1"
-        id="svg-1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        x="0px"
-        y="0px"
-        viewBox="0 0 1400 300"
-        style="enable-background: new 0 0 1400 300"
-        xml:space="preserve"
-      >
-        <path id="XMLID_1_" class="st0" d="M0,273.1" />
-        <path id="XMLID_2_" class="st0" d="M264.1,422.6" />
-        <path
-          id="XMLID_3_"
-          class="st1"
-          d="M0,264c0,0,255-188.6,578.2-114s553.5,150.6,828.3,0l-9,157.5H0"
-        />
-        <path id="XMLID_4_" class="st2" d="M860-173.4" />
-        <polyline
-          id="XMLID_5_"
-          class="st2"
-          points="1406.5,150 1406.5,307.5 1388.3,307.5 "
-        />
-      </svg>
+      <v-row wrap>
+        <v-col cols="12" md="6" class="col">
+          <svg
+            v-if="$vuetify.breakpoint.mdAndUp"
+            version="1.1"
+            id="svg-1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            viewBox="0 0 1400 300"
+            style="enable-background: new 0 0 1400 300"
+            xml:space="preserve"
+          >
+            <path
+              id="XMLID_2_"
+              class="st0"
+              d="M0,294.6c0,0,76-99.2,379.2-88.2c166.7,6.1,249.5,91.9,472.7,39.7
+	c260.7-60.9,548.1,39.6,548.1,39.6v74.9L0,351.9"
+            />
+          </svg>
 
-      <div class="top-left">
-        <div class="group-left">
-          <img class="logo" src="../assets/logo/tacna.png" alt="logo-img" />
-          <ul>
-            <li>S</li>
-            <li>v</li>
-            <li>e</li>
-            <li class="space"></li>
-            <li>j</li>
-            <li>e</li>
-            <li class="space"></li>
-            <li>n</li>
-            <li>a</li>
-            <li class="space"></li>
-            <li>t</li>
-            <li>a</li>
-            <li>c</li>
-            <li>n</li>
-            <li>i</li>
-          </ul>
-          <button class="dobrodosli">DOBRODOŠLI</button>
-        </div>
-      </div>
-      <div class="top-right">
-        <div class="box-right">
-          <p class="intro-paragraf naslov">Da li ste znali?</p>
-          <p class="intro-paragraf">
-            Da 8 od 10 osoba uspešno koristi internet za promovisanje svojih
-            proizvoda i usluga!
-          </p>
-          <br />
-          <p class="intro-paragraf">
-            Da 9 od 10 osoba koristi internet kao sredstvo informisanja!
-          </p>
-          <br />
-          <p class="intro-paragraf">Da li vaši sugradjani znaju šta nudite?</p>
-        </div>
-      </div>
+          <div class="top-left">
+            <div class="group-left">
+              <img class="logo" src="../assets/logo/tacna.png" alt="logo-img" />
+              <ul>
+                <li>S</li>
+                <li>v</li>
+                <li>e</li>
+                <li class="space"></li>
+                <li>j</li>
+                <li>e</li>
+                <li class="space"></li>
+                <li>n</li>
+                <li>a</li>
+                <li class="space"></li>
+                <li>t</li>
+                <li>a</li>
+                <li>c</li>
+                <li>n</li>
+                <li>i</li>
+              </ul>
+              <button class="dobrodosli" v-if="$vuetify.breakpoint.mdAndUp">
+                DOBRODOŠLI
+              </button>
+            </div>
+          </div>
+        </v-col>
+
+        <v-col cols="12" md="6" class="col">
+          <div
+            class="top-right"
+            :class="$vuetify.breakpoint.smAndDown ? `top-right-sm` : ``"
+          >
+            <!-- TERNARY OPERATOR sa vise uslova (uslova) -->
+            <div
+              class="group-right"
+              :class="
+                $vuetify.breakpoint.smAndDown
+                  ? `group-right-sm`
+                  : $vuetify.breakpoint.md
+                  ? `group-right-md`
+                  : `group-right-lg`
+              "
+            >
+              <!-- :style="$vuetify.breakpoint.lg ? " -->
+              <p
+                class="intro-paragraf-naslov"
+                :class="
+                  $vuetify.breakpoint.smAndDown
+                    ? `intro-paragraf-naslov-sm`
+                    : $vuetify.breakpoint.md
+                    ? `intro-paragraf-naslov-md`
+                    : `intro-paragraf-naslov-lg`
+                "
+              >
+                Da li ste znali?
+              </p>
+              <p
+                class="intro-paragraf"
+                :class="
+                  $vuetify.breakpoint.smAndDown
+                    ? `intro-paragraf-sm`
+                    : $vuetify.breakpoint.md
+                    ? `intro-paragraf-md`
+                    : `intro-paragraf-lg`
+                "
+              >
+                8 od 10 osoba uspešno koristi internet za promovisanje svojih
+                proizvoda i usluga!
+              </p>
+              <br />
+              <p
+                class="intro-paragraf"
+                :class="
+                  $vuetify.breakpoint.smAndDown
+                    ? `intro-paragraf-sm`
+                    : $vuetify.breakpoint.md
+                    ? `intro-paragraf-md`
+                    : `intro-paragraf-lg`
+                "
+              >
+                9 od 10 osoba koristi internet kao sredstvo informisanja!
+              </p>
+              <br />
+              <p
+                class="intro-paragraf"
+                :class="
+                  $vuetify.breakpoint.smAndDown
+                    ? `intro-paragraf-sm`
+                    : $vuetify.breakpoint.md
+                    ? `intro-paragraf-md`
+                    : `intro-paragraf-lg`
+                "
+              >
+                Da li vaši sugradjani znaju šta nudite?
+              </p>
+              <button class="dobrodosli" v-if="$vuetify.breakpoint.smAndDown">
+                DOBRODOŠLI
+              </button>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
     </div>
     <div class="bottom"></div>
-  </div>
+  </v-container>
 </template>
 
 <script>
