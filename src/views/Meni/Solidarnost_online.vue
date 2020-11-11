ba02940f5543bdb3c9d3e40a597d282a104e5a4b
kirk7183 <vasicigorjsp@gmail.com>
2020-11-11 14:21:55 +0100

    solidarnost-informacije sredjeno

--- src/views/Meni/Solidarnost_online.vue
+++ src/views/Meni/Solidarnost_online.vue
@@ -2,8 +2,9 @@
   <div class="solidatnost_online">
     <v-app-bar class="app-bar" color="gray" elevation="1" height="50">
       <div v-for="(single, i) in icons" :key="i">
+        <!--1st breakpoint buttons -->
         <v-btn
-          class="spin circle"
+          v-if="$vuetify.breakpoint.smAndDown"
           icon
           elevation="2"
           :class="iconColor_selected[i]"
@@ -11,6 +12,21 @@
         >
           <v-icon class="solidarnost_icon">{{ single.icon }}</v-icon>
         </v-btn>
+
+        <!--2nd breakpoint buttons -->
+        <v-chip
+          class="ma-2 pa-4"
+          v-if="$vuetify.breakpoint.mdAndUp"
+          icon
+          elevation="2"
+          :class="iconColor_selected[i]"
+          @click="changePage(single, i)"
+        >
+          <v-avatar left>
+            <v-icon class="pa-5" size="24">{{ single.icon }}</v-icon>
+          </v-avatar>
+          {{ single.text }}
+        </v-chip>
       </div>
     </v-app-bar>
     <transition name="fade" mode="out-in">
@@ -25,10 +41,26 @@ export default {
     return {
       activePage: 1,
       icons: [
-        { icon: "fas fa-info", route: "/solidarnost_online/info_so" },
-        { icon: "fas fa-star", route: "/solidarnost_online/obavestenje_so" },
-        { icon: "fas fa-gavel", route: "/solidarnost_online/licitacija_so" },
-        { icon: "fas fa-piggy-bank", route: "/solidarnost_online/banka_so" },
+        {
+          icon: "fas fa-info",
+          route: "/solidarnost_online/info_so",
+          text: "Informacije",
+        },
+        {
+          icon: "fas fa-star",
+          route: "/solidarnost_online/obavestenje_so",
+          text: "Obaveštenja",
+        },
+        {
+          icon: "fas fa-gavel",
+          route: "/solidarnost_online/licitacija_so",
+          text: "Licitacije",
+        },
+        {
+          icon: "fas fa-piggy-bank",
+          route: "/solidarnost_online/banka_so",
+          text: "Banka",
+        },
       ],
     };
   },
@@ -36,7 +68,6 @@ export default {
     //da prepozna po URL-u i da postavi broj activePage-a
     if (this.$route.path == "/solidarnost_online/info_so") {
       this.activePage = 1;
-      console.log("info_so");
     } else if (this.$route.path == "/solidarnost_online/obavestenje_so") {
       this.activePage = 2;
     } else if (this.$route.path == "/solidarnost_online/licitacija_so") {
@@ -62,7 +93,6 @@ export default {
     changePage(single, i) {
       this.$router.push({ path: `${single.route}` });
       this.activePage = i + 1;
-      console.log(this.activePage);
     },
   },
 };
