9a5c5aea030bc76d7484fbee2c62e3374f6c3509
kirk7183 <vasicigorjsp@gmail.com>
2021-03-20 14:37:23 +0100

    prepravke u readme.md

--- src/components/licitacije/Uputstvo_li.vue
+++ src/components/licitacije/Uputstvo_li.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="uputstvo_li">uputstvo_li</div>
+  <div>
+    <div class="uputstvo_li">U pripremi je</div>
+    <div>Funkcionise dodavanje novih licitacija i ocitavanje istih</div>
+    <div>
+      potrebno je namestiti sortiranje na 3 nacina kao i dodati opciju za slike
+      u dodavanju licitacije
+    </div>
+  </div>
 </template>
 
 <script>
